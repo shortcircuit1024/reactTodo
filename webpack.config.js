53f0868752eb0aae55667ead7e870407218e96ce
shortcircuit1024 <shortcircuit1024@gmail.com>
2017-02-14 12:41:37 -0600

    fixes update

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,56 @@
 var webpack = require('webpack');
+var path = require('path');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
-    entry: './app/app.jsx',
-    output: {
-        path: __dirname,
-        filename: './public/bundle.js'
-    },
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        compressor: {
-          warnings: false
-        }
-      })
+  entry: [
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    modulesDirectories: [
+      'node_modules',
+      './app/components',
+      './app/api'
     ],
-    resolve: {
-        root: __dirname,
-        modulesDirectories: [
-          'node_modules',
-          './app/components',
-          './app/api'
-        ],
-        alias: {
-          app: 'app',
-          actions: 'app/actions/actions.jsx',
-          reducers: 'app/reducers/reducers.jsx',
-          configureStore: 'app/store/configureStore.jsx'
-
-        },
-        extensions: ['', '.js', '.jsx']
+    alias: {
+      app: 'app',
+      actions: 'app/actions/actions.jsx',
+      reducers: 'app/reducers/reducers.jsx',
+      configureStore: 'app/store/configureStore.jsx'
     },
-    module: {
-        loaders: [{
-            loader: 'babel-loader',
-            query: {
-                presets: ['react', 'es2015', 'stage-0']
-            },
-            test: /\.jsx?$/,
-            exclude: /(node_modules|bower_components)/
-        }]
-    }
-  };
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
+};
